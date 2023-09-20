a0c1f07909bea5f8a8b0252418e87374a52ff899
krivii <luka.krivec15@gmail.com>
2023-09-20 02:01:11 +0200

    GET+PUT user

--- server/src/routes/admin.js
+++ server/src/routes/admin.js
@@ -1,7 +1,5 @@
 import  express  from "express";
 import mongoose from "mongoose";
-
-import { CategoryModel } from "../models/Categories.js";
 import { UserModel } from "../models/Users.js";
 
 
@@ -9,9 +7,29 @@ import { UserModel } from "../models/Users.js";
 
 const router = express.Router();
 
+router.get("/users/:userId", async (req, res) => {
+    try {
+        const userId = req.params.userId;
+  
+        // Find the user by ID
+        const user = await UserModel.findById(userId);
+  
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+  
+        res.json({ message: "User retrieved successfully", user });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+});
+
+
 
 router.delete("/users/:userId", async (req, res) => {
     try {
+        
       const userId = req.params.userId;
   
       // Find the user by ID and delete it
@@ -28,6 +46,27 @@ router.delete("/users/:userId", async (req, res) => {
     }
   });
 
-export {router as adminRouter};
+// Update user route
+router.put("/users/:userId", async (req, res) => {
+    try {
+    console.log("User found in the database:", user);
+      const userId = req.params.userId;
+      const updates = req.body; 
+  
+      // Find the user by ID and update their information
+      const updatedUser = await UserModel.findByIdAndUpdate(userId, updates, { new: true });
+  
+      if (!updatedUser) {
+        return res.status(404).json({ message: "User not found" });
+      }
+  
+      res.json({ message: "User updated successfully", updatedUser });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+  });
+  
+  export { router as adminRouter };
 
 
