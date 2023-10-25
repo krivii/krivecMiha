40f1abd62e4b5937539f9162eba51fae3936e084
krivii <luka.krivec15@gmail.com>
2023-10-25 14:33:24 +0200

    added licence to server

--- server/src/middleware/authorization.js
+++ server/src/middleware/authorization.js
@@ -1,3 +1,7 @@
+//Spletna aplikacija za fotografe/ Web application for photographers
+
+//Copyright (C) 2023  Luka Krivec (lk2378@student.uni-lj.si). Licensed under the GPL-3.0 or later.
+
 import jwt from "jsonwebtoken";
 
 import { UserModel } from "../models/Users.js";
